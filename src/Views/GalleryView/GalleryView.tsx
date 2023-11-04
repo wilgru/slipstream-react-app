6c7dd3461f176fabed531ad1eb8045a5cd18c449
William Gruszka <william.gruszka@gmail.com>
2023-11-04 21:29:35 +1100

    ability to open close and switch between slips

--- src/Views/GalleryView/GalleryView.tsx
+++ src/Views/GalleryView/GalleryView.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SlipCard from "../../components/SlipCard/SlipCard";
 import { Slip } from "../../types/Slip.type";
 import SlipPreview from "../../components/SlipPreview/SlipPreview";
@@ -8,30 +8,114 @@ type GalleryViewProps = {
 };
 
 const GalleryView = ({ slips }: GalleryViewProps) => {
-  const [previewSlip, setPreviewSlip] = useState<Slip | null>(null);
+  const [focusedSlipId, setFocusedSlipId] = useState<string | null>(null);
+  const [openSlip, setOpenSlip] = useState<Slip | null>(null);
 
-  const onPreviewSlip = (selectedSlipId: string) => {
-    setPreviewSlip(slips.find((slip) => slip.id === selectedSlipId) ?? null);
+  const sortedSlips = slips.sort();
+
+  const onClickSlip = (selectedSlipId: string) => {
+    setFocusedSlipId(selectedSlipId);
+    setOpenSlip((prev) => {
+      if (!!prev) {
+        return sortedSlips.find((slip) => slip.id === selectedSlipId) ?? null;
+      } else {
+        return prev;
+      }
+    });
   };
 
-  const slipPreviewOpen = !!previewSlip;
+  const onDblClickSlip = (selectedSlipId: string) => {
+    setOpenSlip((prev) => {
+      if (prev?.id === selectedSlipId) {
+        setFocusedSlipId(null);
+        return null;
+      } else {
+        return sortedSlips.find((slip) => slip.id === selectedSlipId) ?? null;
+      }
+    });
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      console.log(e.key);
+      switch (e.key) {
+        case "ArrowLeft":
+          setFocusedSlipId((prev) => {
+            const focusedSlipIndex = sortedSlips.findIndex(
+              (sortedSlip) => sortedSlip.id === prev
+            );
+
+            const newIndex =
+              focusedSlipIndex - 1 < 0 ? 0 : focusedSlipIndex - 1;
+            const newSlip = sortedSlips[newIndex];
+
+            setOpenSlip((prev) => {
+              return !!prev ? newSlip : prev;
+            });
+
+            return newSlip.id;
+          });
+          break;
+
+        case "ArrowRight":
+          setFocusedSlipId((prev) => {
+            const focusedSlipIndex = sortedSlips.findIndex(
+              (sortedSlip) => sortedSlip.id === prev
+            );
+
+            const newIndex =
+              focusedSlipIndex + 1 > sortedSlips.length - 1
+                ? sortedSlips.length - 1
+                : focusedSlipIndex + 1;
+            const newSlip = sortedSlips[newIndex];
+
+            setOpenSlip((prev) => {
+              return !!prev ? newSlip : prev;
+            });
+
+            return newSlip.id;
+          });
+          break;
+
+        case " ":
+          setOpenSlip((prev) => {
+            return !prev
+              ? sortedSlips.find(
+                  (sortedSlip) => sortedSlip.id === focusedSlipId
+                ) ?? null
+              : null;
+          });
+          break;
+
+        default:
+          break;
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown, true);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown, true);
+    };
+  }, [focusedSlipId]);
 
   return (
     <div className="flex flex-col h-full p-3 gap-3">
       <div
         className={`flex ${
-          slipPreviewOpen ? "overflow-x-auto overflow-y-hidden" : "flex-wrap"
+          !!openSlip ? "overflow-x-auto overflow-y-hidden" : "flex-wrap"
         } gap-3`}
       >
-        {slips.map((slip) => (
+        {sortedSlips.map((slip) => (
           <SlipCard
             slip={slip}
-            active={previewSlip ? slip.id === previewSlip.id : false}
-            onPreviewSlip={onPreviewSlip}
+            isFocused={focusedSlipId ? slip.id === focusedSlipId : false}
+            onClick={onClickSlip}
+            onDblClick={onDblClickSlip}
           />
         ))}
       </div>
-      {slipPreviewOpen && <SlipPreview slip={previewSlip} />}
+      {!!openSlip && <SlipPreview slip={openSlip} />}
     </div>
   );
 };
