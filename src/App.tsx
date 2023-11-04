6ab0fbf39540b181aeb141af4aa7a635c975f391
William Gruszka <william.gruszka@gmail.com>
2023-11-04 21:29:54 +1100

    better test data

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,33 @@ import { Slip } from "./types/Slip.type";
 // const pb = new PocketBase("http://127.0.0.1:8090");
 
 function App() {
-  const length = 100;
-  let slips: Slip[] = [];
-
-  for (let i = 0; i < length; i++) {
-    slips.push({
+  let slips: Slip[] = [
+    {
       id: Math.random().toString(),
       title: "Gartner Hype Cycle",
       content:
         "Is a graphical presentation developed, used and branded by the American research, advisory and information technology firm Gartner to represent the maturity, adoption, and social application of specific technologies. The hype cycle claims to provide a graphical and conceptual presentation of the maturity of emerging technologies through five phases. ",
       isPinned: false,
-    });
-  }
+    },
+    {
+      id: Math.random().toString(),
+      title: "Note 2",
+      content: "This is but a 2nd note...",
+      isPinned: false,
+    },
+    {
+      id: Math.random().toString(),
+      title: "Empty Note",
+      content: null,
+      isPinned: false,
+    },
+    {
+      id: Math.random().toString(),
+      title: "Free Shavocado",
+      content: "'Fre- free shavocado?'",
+      isPinned: false,
+    },
+  ];
 
   return (
     <body>
