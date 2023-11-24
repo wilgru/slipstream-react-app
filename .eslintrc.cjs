3fb1ac0946b58891902b4af925de44fe9e08a3d5
William Gruszka <william.gruszka@gmail.com>
2023-11-24 21:46:57 +1100

    configured linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,46 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: [
+    "dist",
+    ".eslintrc.cjs",
+    "node_modules",
+    "pb_data",
+    "pb_migrations",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "import"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { prefer: "type-imports" },
+    ],
+
+    //import plugin rules
+    "import/no-unresolved": 0,
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        alphabetize: { order: "asc" },
+      },
+    ],
   },
-}
+};
