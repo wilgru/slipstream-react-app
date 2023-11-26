c48587578ec63eccbce81fe186d3ea3b80e3c68b
William Gruszka <william.gruszka@gmail.com>
2023-11-26 19:27:54 +1100

    Revert "changed import paths to begin with src/"
    This reverts commit 9cb7a5a5699ce47013c3ad40751699fd08a5ea88.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { resolve } from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      src: resolve(__dirname, "src"),
-    },
-  },
-});
+})
