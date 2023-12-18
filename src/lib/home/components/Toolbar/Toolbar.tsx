4db8ffe5694799529f95b1db27fd6b8b1a4314d6
William Gruszka <william.gruszka@gmail.com>
2023-12-18 19:30:38 +1100

    moved layout files to home

--- src/lib/home/components/Toolbar/Toolbar.tsx
+++ src/lib/home/components/Toolbar/Toolbar.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { useAuthentication } from "src/lib/authentication/hooks/useAuthentication";
 import { Button } from "src/lib/shared/components/Button/Button";
 import { Search } from "src/lib/shared/components/Search/Search";
@@ -8,12 +9,18 @@ type ToolbarProps = {
 
 export const Toolbar = ({ onClickNewSlipButton }: ToolbarProps) => {
   const { logout } = useAuthentication();
+  const navigate = useNavigate();
+
+  const onClickLogOut = () => {
+    logout();
+    navigate("/login");
+  };
 
   return (
     <div className="flex flex-row justify-between w-full p-3">
       <div className="rhs flex flex-row gap-3"></div>
       <div className="rhs flex flex-row gap-3">
-        <Button onClick={logout}>Log Out</Button>
+        <Button onClick={onClickLogOut}>Log Out</Button>
         <Button onClick={onClickNewSlipButton}>New Slip</Button>
         <Search />
       </div>
