2b60df319db0a8de18885c4aad3e0d1944f6f160
William Gruszka <william.gruszka@gmail.com>
2023-12-26 11:02:07 +1100

    added sidebar component

--- src/home/components/Toolbar/Toolbar.tsx
+++ src/home/components/Toolbar/Toolbar.tsx
@@ -4,10 +4,16 @@ import { Button } from "src/common/components/Button/Button";
 import { Search } from "src/common/components/Search/Search";
 
 type ToolbarProps = {
+  onClickShowSidebarToggle: () => void;
+  showSidebar: boolean;
   onClickNewSlipButton: () => void;
 };
 
-export const Toolbar = ({ onClickNewSlipButton }: ToolbarProps) => {
+export const Toolbar = ({
+  onClickShowSidebarToggle,
+  showSidebar,
+  onClickNewSlipButton,
+}: ToolbarProps) => {
   const { logout } = useAuthentication();
   const navigate = useNavigate();
 
@@ -18,8 +24,12 @@ export const Toolbar = ({ onClickNewSlipButton }: ToolbarProps) => {
 
   return (
     <div className="flex flex-row justify-between w-full p-3">
-      <div className="rhs flex flex-row gap-3"></div>
-      <div className="rhs flex flex-row gap-3">
+      <div className="flex flex-row gap-3">
+        <Button onClick={onClickShowSidebarToggle}>
+          {showSidebar ? "Hide sidebar" : "Show sidebar"}
+        </Button>
+      </div>
+      <div className="flex flex-row gap-3">
         <Button styleType="link" onClick={onClickLogOut}>
           Log Out
         </Button>
