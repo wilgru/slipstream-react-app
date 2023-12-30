e5004a285466e322a86084b92dcbee660ac2f026
William Gruszka <william.gruszka@gmail.com>
2023-12-30 18:05:40 +1100

    small refactor

--- src/slips/components/SlipPreview/utils/handleEscapeKeyDown.ts
+++ src/slips/components/SlipPreview/utils/handleEscapeKeyDown.ts
@@ -1,7 +1,7 @@
-export const handleEscapeKeyDown = (e: KeyboardEvent) => {
+export const handleEscapeKeyDown = () => {
   // .blur() only exists on HTMLElement, document.activeElement could potentially be an Element
   // https://github.com/Microsoft/TypeScript/issues/5901
-  if (e.key === "Escape" && document.activeElement instanceof HTMLElement) {
+  if (document.activeElement instanceof HTMLElement) {
     document.activeElement.blur();
   }
 };
