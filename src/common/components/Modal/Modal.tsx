444e9c05789d96da96a0b12a0774462ca465692b
William Gruszka <william.gruszka@gmail.com>
2024-01-04 19:07:03 +1100

    changed hidden prop

--- src/common/components/Modal/Modal.tsx
+++ src/common/components/Modal/Modal.tsx
@@ -1,39 +1,42 @@
 import { Button } from "../Button/Button";
 
 type ModalProps = {
-  hidden: boolean;
+  visible: boolean;
   title: string;
   closeButton: string;
   saveButton: string;
+  onClose?: () => void;
+  onSave?: () => void;
   children?: JSX.Element;
 };
 
 export const Modal = ({
-  hidden,
+  visible,
   title,
   closeButton,
+  onClose,
   saveButton,
+  onSave,
   children,
 }: ModalProps) => {
   return (
     <div
       className={`${
-        hidden ? "hidden" : ""
+        visible ? "" : "hidden"
       } fixed flex justify-center items-center top-0 right-0 left-0 z-50 bg-stone-700 bg-opacity-75 w-full h-full`}
     >
       <div className="relative flex flex-col p-3 gap-3 w-96 bg-stone-100 border border-stone-700">
-        {/* Modal header */}
         <div className="flex items-center justify-between">
           <h1 className="text-xl text-stone-700 font-medium">{title}</h1>
         </div>
 
-        {/* Modal body */}
         <div>{children}</div>
 
-        {/* Modal footer */}
         <div className="flex gap-3 items-center justify-start">
-          <Button>{saveButton}</Button>
-          <Button styleType="block-outline">{closeButton}</Button>
+          <Button onClick={onSave}>{saveButton}</Button>
+          <Button styleType="block-outline" onClick={onClose}>
+            {closeButton}
+          </Button>
         </div>
       </div>
     </div>
