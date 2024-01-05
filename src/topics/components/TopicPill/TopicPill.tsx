86ca02ae519758a6314ce1eccbf271f2070124ec
William Gruszka <william.gruszka@gmail.com>
2024-01-05 17:17:50 +1100

    added close button on hover

--- src/topics/components/TopicPill/TopicPill.tsx
+++ src/topics/components/TopicPill/TopicPill.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { Button } from "src/common/components/Button/Button";
+
 type TopicPillProps = {
   size?: "medium";
   name: string;
@@ -11,11 +14,24 @@ export const TopicPill = ({
   size = "medium",
   name,
 }: TopicPillProps): JSX.Element => {
+  const [closeBtnVisible, setCloseBtnVisible] = useState<boolean>(false);
+
   const topicPillBaseStyle =
-    "border border-stone-700 bg-stone-300 text-stone-700 text-xs";
+    "flex items-center gap-1 border border-stone-700 bg-stone-300 text-stone-700 text-xs";
   const topicPillSize = TopicPillSize[size];
 
   const topicPillStyles = [topicPillBaseStyle, topicPillSize].join(" ");
 
-  return <div className={topicPillStyles}>{name}</div>;
+  return (
+    <div
+      className={topicPillStyles}
+      onMouseOver={() => setCloseBtnVisible(true)}
+      onMouseOut={() => setCloseBtnVisible(false)}
+    >
+      {name}
+      {closeBtnVisible && (
+        <Button styleType="icon" icon="close" iconSize="small" />
+      )}
+    </div>
+  );
 };
