4239f9ab253eaf1a9cd7744cfe7a69df979e4cb1
William Gruszka <william.gruszka@gmail.com>
2024-01-06 21:38:59 +1100

    added comfirm modal

--- src/topics/components/TopicList/TopicList.tsx
+++ src/topics/components/TopicList/TopicList.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button } from "src/common/components/Button/Button";
+import { Modal } from "src/common/components/Modal/Modal";
 import { TopicPill } from "src/topics/components/TopicPill/TopicPill";
 import type { Topic } from "src/topics/types/Topic.type";
 
@@ -9,22 +10,28 @@ type TopicsListProps = {
 
 type TopicsListItemProps = {
   topic: Topic;
+  onClickDelete: (topic: Topic) => void;
 };
 
-const TopicListItem = ({ topic }: TopicsListItemProps) => {
-  const [closeBtnVisible, setCloseBtnVisible] = useState<boolean>(false);
+const TopicListItem = ({ topic, onClickDelete }: TopicsListItemProps) => {
+  const [deleteBtnVisible, setDeleteBtnVisible] = useState<boolean>(false);
 
   return (
     <div
       className="flex justify-between items-center"
-      onMouseOver={() => setCloseBtnVisible(true)}
-      onMouseLeave={() => setCloseBtnVisible(false)}
+      onMouseOver={() => setDeleteBtnVisible(true)}
+      onMouseLeave={() => setDeleteBtnVisible(false)}
     >
       <TopicPill name={topic.name} />
 
       <div className="flex gap-2">
-        {closeBtnVisible && (
-          <Button styleType="icon" icon="close" iconSize="small" />
+        {deleteBtnVisible && (
+          <Button
+            styleType="icon"
+            icon="close"
+            iconSize="small"
+            onClick={() => onClickDelete(topic)}
+          />
         )}
         <p className="text-xs text-stone-500 w-2 text-center">
           {topic.slipCount}
@@ -35,11 +42,33 @@ const TopicListItem = ({ topic }: TopicsListItemProps) => {
 };
 
 export const TopicList = ({ topics }: TopicsListProps): JSX.Element => {
+  const [topicToDelete, setTopicToDelete] = useState<Topic | undefined>(
+    undefined
+  );
+
   return (
-    <div className="flex flex-col gap-2">
-      {topics.map((topic) => (
-        <TopicListItem topic={topic}></TopicListItem>
-      ))}
-    </div>
+    <>
+      <div className="flex flex-col gap-2">
+        {topics.map((topic) => (
+          <TopicListItem
+            topic={topic}
+            onClickDelete={setTopicToDelete}
+          ></TopicListItem>
+        ))}
+      </div>
+
+      <Modal
+        visible={!!topicToDelete}
+        title={"Confirm delete topic"}
+        closeButton={"Cancel"}
+        saveButton={"Confirm"}
+        onSave={() => setTopicToDelete(undefined)}
+        onClose={() => setTopicToDelete(undefined)}
+      >
+        <p className="text-xs">
+          Are you sure you want to delete '{topicToDelete?.name}'?
+        </p>
+      </Modal>
+    </>
   );
 };
