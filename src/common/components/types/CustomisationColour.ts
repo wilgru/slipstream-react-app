ee922fff4647b739b8a7ec82aee0692fa69a309b
William Gruszka <william.gruszka@gmail.com>
2024-01-17 20:46:58 +1100

    added initial customisation colours

---
+++ src/common/components/types/CustomisationColour.ts
@@ -0,0 +1,14 @@
+export enum CustomisationColour {
+  "red" = "red-500",
+  "orange" = "orange-500",
+  "yellow" = "yellow-300",
+  "lime" = "lime-300",
+  "green" = "green-500",
+  "blue" = "blue-500",
+  "cyan" = "cyan-300",
+  "pink" = "pink-300",
+  "purple" = "purple-500",
+  "brown" = "amber-700",
+  "grey" = "gray-500",
+  "default" = "stone-700",
+}
--- src/topics/types/Topic.type.ts
+++ src/topics/types/Topic.type.ts
@@ -1,5 +1,8 @@
+import type { CustomisationColour } from "src/common/components/types/CustomisationColour";
+
 export type Topic = {
   id: string;
   name: string;
+  colour: CustomisationColour;
   slipCount?: number;
 };
