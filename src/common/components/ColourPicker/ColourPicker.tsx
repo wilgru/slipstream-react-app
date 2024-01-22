bc37aca9f2b4c1df9b9b3fb2958b0d639a3f5e51
William Gruszka <william.gruszka@gmail.com>
2024-01-23 08:04:11 +1100

    moved customisationColours to colour picker component

--- src/common/components/ColourPicker/ColourPicker.tsx
+++ src/common/components/ColourPicker/ColourPicker.tsx
@@ -1,38 +1,54 @@
-import { TopicColour } from "src/topics/types/TopicColour.type";
+import { type Colour } from "src/common/components/types/Colour";
+
+// these colours are manually included with the build via the tailwind.config.js
+const customisationColours: Colour[] = [
+  { name: "red", primary: "red-500", secondary: "" },
+  { name: "orange", primary: "orange-500", secondary: "" },
+  { name: "yellow", primary: "yellow-300", secondary: "" },
+  { name: "lime", primary: "lime-300", secondary: "" },
+  { name: "green", primary: "green-500", secondary: "" },
+  { name: "blue", primary: "blue-500", secondary: "" },
+  { name: "cyan", primary: "cyan-300", secondary: "" },
+  { name: "pink", primary: "pink-300", secondary: "" },
+  { name: "purple", primary: "purple-500", secondary: "" },
+  { name: "brown", primary: "amber-700", secondary: "" },
+  { name: "grey", primary: "gray-500", secondary: "" },
+  { name: "default", primary: "stone-700", secondary: "" },
+];
 
 const ColourBtn = ({
   colour,
   selected,
   onClick,
 }: {
-  colour: TopicColour;
+  colour: Colour;
   selected: boolean;
-  onClick: (colour: TopicColour) => void;
+  onClick: (colour: Colour) => void;
 }) => {
   return (
     <button
       onClick={() => onClick(colour)}
-      style={{ backgroundColor: colour }}
-      className={`h-8 w-8 ${selected && "border-2 border-stone-700"}`}
+      style={{ backgroundColor: colour.primary }}
+      className={`h-8 w-8 ${selected && "border-2 border-stone-700"} bg-${
+        colour.primary
+      }`}
     ></button>
   );
 };
 
 export const ColourPicker = ({
-  colours = [TopicColour["green"], TopicColour["red"]],
-  selectedColour,
+  selectedColourName,
   onSelectColour,
 }: {
-  colours?: TopicColour[];
-  selectedColour: TopicColour;
-  onSelectColour: (colour: TopicColour) => void;
+  selectedColourName: string;
+  onSelectColour: (colour: Colour) => void;
 }) => {
   return (
     <div className="flex flex-wrap gap-2">
-      {colours.map((colour) => (
+      {customisationColours.map((customisationColour) => (
         <ColourBtn
-          colour={colour}
-          selected={selectedColour === colour}
+          colour={customisationColour}
+          selected={selectedColourName === customisationColour.name}
           onClick={onSelectColour}
         />
       ))}
---
+++ src/common/components/types/Colour.ts
@@ -0,0 +1,1 @@
+export type Colour = { name: string; primary: string; secondary: string };
