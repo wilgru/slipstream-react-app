691c4bad8aefdb009c908c365030e3eee127a223
William Gruszka <william.gruszka@gmail.com>
2024-01-23 19:23:17 +1100

    updated components to have customisable colours

--- src/common/components/Toggle/Toggle.tsx
+++ src/common/components/Toggle/Toggle.tsx
@@ -6,15 +6,16 @@ type ToggleProps = {
   iconSize?: "small" | "medium" | "large";
   iconToggledOnColour?: string;
   styleType?: "block" | "icon";
-  size?: "small" | "medium" | "large";
+  colour?: { border: string; background: string; text: string };
   width?: "fit" | "full";
+  size?: "small" | "medium" | "large";
   disabled?: boolean;
   onClick?: () => void;
   isToggled: boolean;
 };
 
 enum ToggleStyleType {
-  "block" = "border border-stone-700 font-medium hover:bg-stone-800 hover:border-stone-800 hover:text-stone-100",
+  "block" = "border font-medium hover:bg-stone-800 hover:border-stone-800 hover:text-stone-100",
   "icon" = "",
 }
 
@@ -29,20 +30,13 @@ enum ToggleWidth {
   "fit" = "",
 }
 
-enum ToggleOnColour {
-  "block" = "bg-stone-700 text-stone-100",
-}
-
-enum ToggleOffColour {
-  "block" = "bg-stone-100 text-stone-700",
-}
-
 export const Toggle = ({
   children,
   icon,
   iconSize = "medium",
   iconToggledOnColour = "stone-700", // TODO make required only if is icon style type
   styleType = "block",
+  colour = { border: "stone-700", background: "stone-100", text: "stone-100" },
   width = "fit",
   size = "medium",
   disabled = false,
@@ -52,16 +46,23 @@ export const Toggle = ({
   const toggleBaseStyle =
     "text-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500";
   const toggleStyleType = ToggleStyleType[styleType];
+  const toggleColourStyle =
+    styleType === "block" ? `border-${colour.border}` : "";
   const toggleWidth = ToggleWidth[width];
   const toggleSize = styleType === "icon" ? "" : ToggleSize[size];
   const toggleToggledOnColour =
-    styleType === "icon" ? iconToggledOnColour : ToggleOnColour[styleType];
+    styleType === "icon"
+      ? iconToggledOnColour
+      : `bg-${colour.border} text-stone-100`;
   const toggleToggledOffColour =
-    styleType === "icon" ? "stone-500" : ToggleOffColour[styleType];
+    styleType === "icon"
+      ? "stone-500"
+      : `bg-${colour.background} text-stone-700`;
 
   const toggleStyles = [
     toggleBaseStyle,
     toggleStyleType,
+    toggleColourStyle,
     toggleWidth,
     toggleSize,
     isToggled ? toggleToggledOnColour : toggleToggledOffColour,
--- src/topics/components/TopicPill/TopicPill.tsx
+++ src/topics/components/TopicPill/TopicPill.tsx
@@ -1,25 +1,37 @@
 import { useState } from "react";
 import { Button } from "src/common/components/Button/Button";
 import { Toggle } from "src/common/components/Toggle/Toggle";
+import { customisationColours } from "src/common/constants/customisationColours";
+import type { Topic } from "src/topics/types/Topic.type";
 
 type TopicPillProps = {
-  id: string;
-  name: string;
+  topic: Topic;
   size?: "small";
   closable?: boolean;
-  isSelected: boolean;
+  isSelected?: boolean;
   onClick?: (id: string) => void;
 };
 
 export const TopicPill = ({
-  id,
-  name,
+  topic,
   size = "small",
   closable = false,
-  isSelected,
+  isSelected = false,
   onClick,
 }: TopicPillProps): JSX.Element => {
   const [closeBtnVisible, setCloseBtnVisible] = useState<boolean>(false);
+
+  const topicColour = customisationColours.find(
+    (colour) => colour.name === topic.colour
+  );
+
+  const buttonColour = topicColour
+    ? {
+        border: topicColour.primary,
+        background: topicColour.secondary,
+        text: topicColour.lightText ? "stone-100" : "stone-700",
+      }
+    : { border: "stone-700", background: "stone-300", text: "stone-700" };
 
   return (
     <div
@@ -28,23 +40,25 @@ export const TopicPill = ({
     >
       {closable ? (
         <Button
-          styleType="block-outline"
+          styleType="block"
+          colour={buttonColour}
           size={size}
           icon={closable && closeBtnVisible ? "close" : ""}
           iconSize={size}
-          onClick={() => onClick && onClick(id)}
+          onClick={() => onClick && onClick(topic.id)}
         >
-          {name}
+          {topic.name}
         </Button>
       ) : (
         <Toggle
           styleType="block"
+          colour={buttonColour}
           size={size}
           iconSize={size}
-          onClick={() => onClick && onClick(id)}
+          onClick={() => onClick && onClick(topic.id)}
           isToggled={isSelected}
         >
-          {name}
+          {topic.name}
         </Toggle>
       )}
     </div>
