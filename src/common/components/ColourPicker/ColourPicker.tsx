0cf5e302e41f236f57cf6be1c0a240abb0cda54c
William Gruszka <william.gruszka@gmail.com>
2024-01-23 19:19:28 +1100

    added component prop types

--- src/common/components/ColourPicker/ColourPicker.tsx
+++ src/common/components/ColourPicker/ColourPicker.tsx
@@ -1,35 +1,23 @@
-import { type Colour } from "src/common/components/types/Colour";
+import { customisationColours } from "src/common/constants/customisationColours";
+import { type Colour } from "src/common/types/Colour";
 
-// these colours are manually included with the build via the tailwind.config.js
-const customisationColours: Colour[] = [
-  { name: "red", primary: "red-500", secondary: "" },
-  { name: "orange", primary: "orange-500", secondary: "" },
-  { name: "yellow", primary: "yellow-300", secondary: "" },
-  { name: "lime", primary: "lime-300", secondary: "" },
-  { name: "green", primary: "green-500", secondary: "" },
-  { name: "blue", primary: "blue-500", secondary: "" },
-  { name: "cyan", primary: "cyan-300", secondary: "" },
-  { name: "pink", primary: "pink-300", secondary: "" },
-  { name: "purple", primary: "purple-500", secondary: "" },
-  { name: "brown", primary: "amber-700", secondary: "" },
-  { name: "grey", primary: "gray-500", secondary: "" },
-  { name: "default", primary: "stone-700", secondary: "" },
-];
-
-const ColourBtn = ({
-  colour,
-  selected,
-  onClick,
-}: {
+type ColourBtnProps = {
   colour: Colour;
   selected: boolean;
   onClick: (colour: Colour) => void;
-}) => {
+};
+
+type ColourPickerProps = {
+  selectedColourName: string;
+  onSelectColour: (colour: Colour) => void;
+};
+
+const ColourBtn = ({ colour, selected, onClick }: ColourBtnProps) => {
   return (
     <button
       onClick={() => onClick(colour)}
       style={{ backgroundColor: colour.primary }}
-      className={`h-8 w-8 ${selected && "border-2 border-stone-700"} bg-${
+      className={`h-8 w-8 ${selected && "border-2 border-orange-500"} bg-${
         colour.primary
       }`}
     ></button>
@@ -39,10 +27,7 @@ const ColourBtn = ({
 export const ColourPicker = ({
   selectedColourName,
   onSelectColour,
-}: {
-  selectedColourName: string;
-  onSelectColour: (colour: Colour) => void;
-}) => {
+}: ColourPickerProps) => {
   return (
     <div className="flex flex-wrap gap-2">
       {customisationColours.map((customisationColour) => (
