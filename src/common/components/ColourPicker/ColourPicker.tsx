eb93feb10c0303c8cf67419ea42c28dd4ee7d390
William Gruszka <william.gruszka@gmail.com>
2024-01-25 20:36:37 +1100

    moved colour picker button to own component

--- src/common/components/ColourPicker/ColourPicker.tsx
+++ src/common/components/ColourPicker/ColourPicker.tsx
@@ -1,27 +1,10 @@
 import { customisationColours } from "src/common/constants/customisationColours";
 import { type Colour } from "src/common/types/Colour";
-
-type ColourBtnProps = {
-  colour: Colour;
-  selected: boolean;
-  onClick: (colour: Colour) => void;
-};
+import { ColourPickerColourButton } from "./ColourPickerColourButton";
 
 type ColourPickerProps = {
   selectedColourName: string;
   onSelectColour: (colour: Colour) => void;
-};
-
-const ColourBtn = ({ colour, selected, onClick }: ColourBtnProps) => {
-  return (
-    <button
-      onClick={() => onClick(colour)}
-      style={{ backgroundColor: colour.primary }}
-      className={`h-8 w-8 ${selected && "border-2 border-orange-500"} bg-${
-        colour.primary
-      }`}
-    ></button>
-  );
 };
 
 export const ColourPicker = ({
@@ -31,7 +14,7 @@ export const ColourPicker = ({
   return (
     <div className="flex flex-wrap gap-2">
       {customisationColours.map((customisationColour) => (
-        <ColourBtn
+        <ColourPickerColourButton
           colour={customisationColour}
           selected={selectedColourName === customisationColour.name}
           onClick={onSelectColour}
