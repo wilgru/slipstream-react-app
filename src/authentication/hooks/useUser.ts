90c0254282550f378ad11efaaa7c1ce739ba68f7
William Gruszka <william.gruszka@gmail.com>
2024-05-04 10:15:29 +1000

    added comment hehe

--- src/authentication/hooks/useUser.ts
+++ src/authentication/hooks/useUser.ts
@@ -4,7 +4,7 @@ import type { User } from "../types/User.type";
 const currentUserAtom = atom<User | undefined>(undefined);
 
 export const useUser = () => {
-  const [currentUser, setCurrentUser] = useAtom(currentUserAtom);
+  const [currentUser, setCurrentUser] = useAtom(currentUserAtom); //somehow store user data in a query cache/access the data from the cache here instead of an atom?
 
   return { currentUser, setCurrentUser };
 };
