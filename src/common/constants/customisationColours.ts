4777372e152c601bb8125a170cef808b2501fbf6
williamgruszka <william.gruszka@gmail.com>
2024-12-30 10:46:45 +1100

    fixed background colours

--- src/common/constants/customisationColours.ts
+++ src/common/constants/customisationColours.ts
@@ -6,56 +6,51 @@ export const customisationColours: Colour[] = [
   {
     name: "orange",
     textClass: "text-orange-400",
-    backgroundClass: "bg-red-400",
+    backgroundClass: "bg-orange-400",
   },
   {
     name: "yellow",
     textClass: "text-yellow-400",
-    backgroundClass: "bg-red-400",
+    backgroundClass: "bg-yellow-400",
   },
   {
     name: "lime",
     textClass: "text-lime-400",
-    backgroundClass: "bg-red-400",
+    backgroundClass: "bg-lime-400",
   },
   {
     name: "green",
     textClass: "text-green-400",
-    backgroundClass: "bg-red-400",
+    backgroundClass: "bg-green-400",
   },
   {
     name: "blue",
     textClass: "text-blue-400",
-    backgroundClass: "bg-red-400",
+    backgroundClass: "bg-blue-400",
   },
   {
     name: "cyan",
     textClass: "text-cyan-400",
-    backgroundClass: "bg-red-400",
+    backgroundClass: "bg-cyan-400",
   },
   {
     name: "pink",
     textClass: "text-pink-400",
-    backgroundClass: "bg-red-400",
+    backgroundClass: "bg-pink-400",
   },
   {
     name: "purple",
     textClass: "text-purple-400",
-    backgroundClass: "bg-red-400",
+    backgroundClass: "bg-purple-400",
   },
   {
     name: "brown",
     textClass: "text-amber-600",
-    backgroundClass: "bg-red-400",
+    backgroundClass: "bg-amber-400",
   },
   {
     name: "grey",
     textClass: "text-gray-400",
-    backgroundClass: "bg-red-400",
-  },
-  {
-    name: "default",
-    textClass: "text-stone-600",
-    backgroundClass: "bg-red-400",
+    backgroundClass: "bg-gray-400",
   },
 ];
