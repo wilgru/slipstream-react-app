4c23a81135518acb5285194f39b13a0eff278c5d
williamgruszka <william.gruszka@gmail.com>
2025-01-24 08:19:32 +1100

    added divider between slips with and without titles

--- src/journals/JournalPage.tsx
+++ src/journals/JournalPage.tsx
@@ -5,6 +5,7 @@ import { customisationColours } from "src/common/constants/customisationColours"
 import { cn } from "src/common/utils/cn";
 import { useJournal } from "../models/journal/hooks/useJournal";
 import { EditJournalModal } from "./components/EditJournalModal";
+import type { Slip } from "src/models/slip/types/Slip.type";
 
 export default function JournalPage() {
   const { journalId } = useParams<{ journalId: string }>();
@@ -18,6 +19,22 @@ export default function JournalPage() {
   const customisationColour = customisationColours.find(
     (colour) => colour.name === journal.colour
   );
+
+  const sectionedSlips: {
+    noTitles: Slip[];
+    withTitles: Slip[];
+  } = {
+    noTitles: [],
+    withTitles: [],
+  };
+
+  journal.slips?.forEach((slip) => {
+    if (slip.title) {
+      sectionedSlips.withTitles.push(slip);
+    } else {
+      sectionedSlips.noTitles.push(slip);
+    }
+  });
 
   return (
     <div className="h-full w-full overflow-y-scroll z-10">
@@ -48,7 +65,30 @@ export default function JournalPage() {
       </div>
 
       <div className="p-4 mb-4 mx-4 border min-h-full border-stone-300 rounded-lg flex flex-col gap-7 bg-white shadow-light">
-        {journal.slips?.map((slip) => (
+        {sectionedSlips.noTitles.map((slip) => (
+          <div key={slip.id}>
+            <h1
+              className={`select-none font-title text-2xl font-normal tracking-tight text-stone-700`}
+            >
+              {slip.title}
+            </h1>
+
+            <p className="text-sm">
+              {slip.content.reduce((acc, op) => acc + op.insert, "")}
+            </p>
+          </div>
+        ))}
+
+        {sectionedSlips.noTitles.length > 0 &&
+          sectionedSlips.withTitles.length > 0 && (
+            <div className="flex flex-row gap-2 justify-center">
+              <div className=" rounded-full bg-stone-300 h-1 w-1"></div>
+              <div className=" rounded-full bg-stone-300 h-1 w-1"></div>
+              <div className=" rounded-full bg-stone-300 h-1 w-1"></div>
+            </div>
+          )}
+
+        {sectionedSlips.withTitles.map((slip) => (
           <div key={slip.id}>
             <h1
               className={`select-none font-title text-2xl font-normal tracking-tight text-stone-700`}
