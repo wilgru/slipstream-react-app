ecaa5ae19e11e2c99f9dd40240fd3fe28e786aff
williamgruszka <william.gruszka@gmail.com>
2025-01-28 20:52:15 +1100

    tweaked colours

--- src/lib/colour/constants/customisationColours.ts
+++ src/lib/colour/constants/customisationColours.ts
@@ -1,56 +1,56 @@
-import type { Colour } from "src/lib/types/Colour";
+import type { Colour } from "src/lib/colour/types/Colour";
 
 // these colours are manually included with the build via the tailwind.config.js
 export const customisationColours: Colour[] = [
-  { name: "red", textClass: "text-red-400", backgroundClass: "bg-red-400" },
+  { name: "red", textClass: "text-red-500", backgroundClass: "bg-red-100" },
   {
     name: "orange",
-    textClass: "text-orange-400",
-    backgroundClass: "bg-orange-400",
+    textClass: "text-orange-500",
+    backgroundClass: "bg-orange-100",
   },
   {
     name: "yellow",
-    textClass: "text-yellow-400",
-    backgroundClass: "bg-yellow-400",
+    textClass: "text-yellow-500",
+    backgroundClass: "bg-yellow-100",
   },
   {
     name: "lime",
-    textClass: "text-lime-400",
-    backgroundClass: "bg-lime-400",
+    textClass: "text-lime-500",
+    backgroundClass: "bg-lime-100",
   },
   {
     name: "green",
-    textClass: "text-green-400",
-    backgroundClass: "bg-green-400",
+    textClass: "text-green-500",
+    backgroundClass: "bg-green-100",
   },
   {
     name: "blue",
-    textClass: "text-blue-400",
-    backgroundClass: "bg-blue-400",
+    textClass: "text-blue-500",
+    backgroundClass: "bg-blue-100",
   },
   {
     name: "cyan",
-    textClass: "text-cyan-400",
-    backgroundClass: "bg-cyan-400",
+    textClass: "text-cyan-500",
+    backgroundClass: "bg-cyan-100",
   },
   {
     name: "pink",
-    textClass: "text-pink-400",
-    backgroundClass: "bg-pink-400",
+    textClass: "text-pink-500",
+    backgroundClass: "bg-pink-100",
   },
   {
     name: "purple",
-    textClass: "text-purple-400",
-    backgroundClass: "bg-purple-400",
+    textClass: "text-purple-500",
+    backgroundClass: "bg-purple-100",
   },
   {
     name: "brown",
-    textClass: "text-amber-600",
-    backgroundClass: "bg-amber-400",
+    textClass: "text-amber-700",
+    backgroundClass: "bg-amber-200",
   },
   {
     name: "grey",
-    textClass: "text-gray-400",
-    backgroundClass: "bg-gray-400",
+    textClass: "text-gray-500",
+    backgroundClass: "bg-gray-200",
   },
 ];
