2a6901c09d2505b8a09833d1a31ffdd6783be8a5
williamgruszka <william.gruszka@gmail.com>
2025-01-30 08:38:54 +1100

    added flagged filter

--- src/models/slips/hooks/useGetSlips.ts
+++ src/models/slips/hooks/useGetSlips.ts
@@ -1,41 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
-import { useAtomValue } from "jotai";
 import { pb } from "src/connections/pocketbase";
-import { selectedJournalIdsAtom } from "src/models/journals/atoms/selectedJournalIdsAtom";
 import { mapSlip } from "src/models/slips/utils/mapSlip";
 import type { Slip } from "src/models/slips/Slip.type";
 
 type UseGetSlipsResponse = { slips: Slip[] };
 
-export const useGetSlips = (): UseGetSlipsResponse => {
-  const selectedJournalIds = useAtomValue(selectedJournalIdsAtom);
-
+export const useGetSlips = (
+  isFlagged: boolean = false
+): UseGetSlipsResponse => {
   const queryFn = async (): Promise<Slip[]> => {
-    // for some reason cant use && for the multiple journals filters, which is why slipsWithAllJournals and its logic exists
-    const journalsFilter = selectedJournalIds.length
-      ? "&& " +
-        selectedJournalIds
-          .map((journalId) => `journals.id ?= '${journalId}'`)
-          .join(" || ")
-      : "";
+    const isFlaggedFilter = isFlagged ? "&& isFlagged = true" : "";
 
     const rawSlips = await pb
       .collection("slips")
       .getList(undefined, undefined, {
-        filter: `deleted = null ${journalsFilter}`,
+        filter: `deleted = null ${isFlaggedFilter}`,
         expand: "journals",
         sort: "-isPinned",
       });
 
     const mappedSlips = rawSlips.items.map(mapSlip);
 
-    const slipsWithAllJournals = mappedSlips.filter((mappedSlip) =>
-      selectedJournalIds.every((journalId) =>
-        mappedSlip.journals.some((journal) => journal.id === journalId)
-      )
-    );
-
-    return slipsWithAllJournals;
+    return mappedSlips;
   };
 
   // TODO: consider time caching for better performance
