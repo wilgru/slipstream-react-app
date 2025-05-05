3aba6b9d5d96ae2022ad36679d3079d29bc2c90d
William Gruszka <40753609+wilgru@users.noreply.github.com>
2025-05-05 18:44:16 +1000

    Revert "more styling to tag tree"
    This reverts commit a4068cdd486e555a30dc9f9da0fd436ff44f96bb.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,16 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { useNavigate } from "@tanstack/react-router";
-import { useState } from "react";
-import { Tree } from "react-d3-tree";
+import React, { useState } from "react";
+import Tree from "react-d3-tree";
 import { Button } from "src/components/Button/Button";
 import EditSlipModal from "src/components/EditSlipModal/EditSlipModal";
+import { Toggle } from "src/components/Toggle/Toggle";
 import { colours } from "src/constants/colours.constant";
+import { useGetJournals } from "src/hooks/journals/useGetJournals";
 import { useLogin } from "src/hooks/users/useLogin";
 import { cn } from "src/utils/cn";
+import { NavItem } from "./NavItem";
 
 // This is a simplified example of an org chart with a depth of 2.
 // Note how deeper levels are defined recursively via the `children` property.
@@ -44,46 +47,6 @@ const orgChart = {
         },
       ],
     },
-    {
-      name: "Food",
-      attributes: {
-        Slips: "4",
-      },
-      children: [
-        {
-          name: "Cooking",
-          attributes: {
-            Slips: "4",
-          },
-        },
-        {
-          name: "Baking",
-          attributes: {
-            Slips: "4",
-          },
-        },
-      ],
-    },
-    {
-      name: "Nationalities",
-      attributes: {
-        Slips: "4",
-      },
-      children: [
-        {
-          name: "French",
-          attributes: {
-            Slips: "4",
-          },
-        },
-        {
-          name: "Italian",
-          attributes: {
-            Slips: "4",
-          },
-        },
-      ],
-    },
   ],
 };
 
@@ -96,19 +59,13 @@ const renderForeignObjectNode = ({
     <button
       className={cn(
         "w-full flex flex-col items-center justify-center rounded-full p-1",
-        nodeDatum.__rd3t.collapsed ? "bg-white" : "bg-stone-800"
+        nodeDatum.__rd3t.collapsed
+          ? "bg-white text-black"
+          : "bg-black text-white"
       )}
       onClick={toggleNode}
     >
-      <h3
-        className={cn(
-          "text-center",
-          nodeDatum.__rd3t.collapsed ? "text-stone-800" : "text-white"
-        )}
-        style={{ textAlign: "center" }}
-      >
-        {nodeDatum.name}
-      </h3>
+      <h3 style={{ textAlign: "center" }}>{nodeDatum.name}</h3>
       {/* {nodeDatum.children && (
         <button style={{ width: "100%" }} onClick={toggleNode}>
           {nodeDatum.__rd3t.collapsed ? "Expand" : "Collapse"}
@@ -121,14 +78,57 @@ const renderForeignObjectNode = ({
 export const Sidebar = () => {
   const { logout } = useLogin();
   const navigate = useNavigate();
+  const { journals } = useGetJournals();
 
   const [showEditSlipModal, setShowEditSlipModal] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const nodeSize = { x: 100, y: 100 };
   const foreignObjectProps = { width: nodeSize.x, height: nodeSize.y, x: -50 };
 
+  const onClickShowSidebarToggle = (): void => {
+    setExpanded((currentShowSidebar) => !currentShowSidebar);
+  };
+
   return (
     <aside className={"flex flex-col flex-grow justify-between"}>
+      {/* <div className="flex flex-col gap-2 overflow-y-auto">
+        <section className="flex flex-col gap-1">
+          <NavItem
+            ghost
+            iconName="pencil"
+            title={"Stream"}
+            to={`/stream/`}
+            expanded={expanded}
+          />
+
+          <NavItem
+            ghost
+            iconName="flag"
+            title={"Flagged"}
+            to={`/flagged/`}
+            expanded={expanded}
+          />
+        </section>
+
+        <div className="h-[1px] mx-1 bg-stone-300 rounded-full" />
+
+        <section className="flex flex-col gap-1">
+          {expanded && <h1 className="font-title text-lg ml-2">Journals</h1>}
+
+          {journals.map((journal) => (
+            <NavItem
+              iconName={journal.icon}
+              colour={journal.colour}
+              title={journal.name}
+              preview={journal.slipCount}
+              to={`/journals/${journal.id}`}
+              expanded={expanded}
+            />
+          ))}
+        </section>
+      </div> */}
+
       <Tree
         data={orgChart}
         orientation="vertical"
@@ -142,6 +142,12 @@ export const Sidebar = () => {
         <div className="h-[1px] mx-1 bg-stone-300 rounded-full" />
 
         <div className={cn("flex items-center")}>
+          <Toggle
+            onClick={onClickShowSidebarToggle}
+            isToggled={expanded}
+            iconName="sidebarSimple"
+          />
+
           <DropdownMenu.Root>
             <DropdownMenu.Trigger asChild>
               <div>
@@ -168,20 +174,11 @@ export const Sidebar = () => {
             </DropdownMenu.Portal>
           </DropdownMenu.Root>
 
-          <Button
-            variant="ghost"
-            iconName="tag"
-            colour={colours.orange}
-            onClick={() => {
-              setShowEditSlipModal(true);
-            }}
-          />
-
           <Dialog.Root>
             <Dialog.Trigger asChild>
               <Button
                 variant="ghost"
-                iconName="pencil"
+                iconName="plus"
                 colour={colours.orange}
                 onClick={() => {
                   setShowEditSlipModal(true);
