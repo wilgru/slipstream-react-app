8f2241c4ad00ac7db83e9c7d97508bb1650737b2
William Gruszka <40753609+wilgru@users.noreply.github.com>
2025-05-05 18:44:39 +1000

    Revert "added initial tag tree (non functional)"
    This reverts commit 90f1302d52e62fd63533b4ff6c153db82e1c8232.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { useNavigate } from "@tanstack/react-router";
-import React, { useState } from "react";
-import Tree from "react-d3-tree";
+import { useState } from "react";
 import { Button } from "src/components/Button/Button";
 import EditSlipModal from "src/components/EditSlipModal/EditSlipModal";
 import { Toggle } from "src/components/Toggle/Toggle";
@@ -12,69 +11,6 @@ import { useLogin } from "src/hooks/users/useLogin";
 import { cn } from "src/utils/cn";
 import { NavItem } from "./NavItem";
 
-// This is a simplified example of an org chart with a depth of 2.
-// Note how deeper levels are defined recursively via the `children` property.
-const orgChart = {
-  name: "Home",
-  children: [
-    {
-      name: "Art",
-      attributes: {
-        Slips: "4",
-      },
-      children: [
-        {
-          name: "Printing",
-          attributes: {
-            Slips: "4",
-          },
-          children: [
-            {
-              name: "Silkscreen",
-            },
-          ],
-        },
-        {
-          name: "Ceramics",
-          attributes: {
-            Slips: "4",
-          },
-          children: [
-            {
-              name: "Glazing",
-            },
-          ],
-        },
-      ],
-    },
-  ],
-};
-
-const renderForeignObjectNode = ({
-  nodeDatum,
-  toggleNode,
-  foreignObjectProps,
-}) => (
-  <foreignObject {...foreignObjectProps}>
-    <button
-      className={cn(
-        "w-full flex flex-col items-center justify-center rounded-full p-1",
-        nodeDatum.__rd3t.collapsed
-          ? "bg-white text-black"
-          : "bg-black text-white"
-      )}
-      onClick={toggleNode}
-    >
-      <h3 style={{ textAlign: "center" }}>{nodeDatum.name}</h3>
-      {/* {nodeDatum.children && (
-        <button style={{ width: "100%" }} onClick={toggleNode}>
-          {nodeDatum.__rd3t.collapsed ? "Expand" : "Collapse"}
-        </button>
-      )} */}
-    </button>
-  </foreignObject>
-);
-
 export const Sidebar = () => {
   const { logout } = useLogin();
   const navigate = useNavigate();
@@ -83,16 +19,13 @@ export const Sidebar = () => {
   const [showEditSlipModal, setShowEditSlipModal] = useState(false);
   const [expanded, setExpanded] = useState(false);
 
-  const nodeSize = { x: 100, y: 100 };
-  const foreignObjectProps = { width: nodeSize.x, height: nodeSize.y, x: -50 };
-
   const onClickShowSidebarToggle = (): void => {
     setExpanded((currentShowSidebar) => !currentShowSidebar);
   };
 
   return (
-    <aside className={"flex flex-col flex-grow justify-between"}>
-      {/* <div className="flex flex-col gap-2 overflow-y-auto">
+    <aside className={cn("flex flex-col justify-between")}>
+      <div className="flex flex-col gap-2 overflow-y-auto">
         <section className="flex flex-col gap-1">
           <NavItem
             ghost
@@ -127,21 +60,17 @@ export const Sidebar = () => {
             />
           ))}
         </section>
-      </div> */}
-
-      <Tree
-        data={orgChart}
-        orientation="vertical"
-        translate={{ x: 0, y: 0 }}
-        renderCustomNodeElement={(rd3tProps) =>
-          renderForeignObjectNode({ ...rd3tProps, foreignObjectProps })
-        }
-      />
+      </div>
 
       <div className="flex flex-col gap-2">
         <div className="h-[1px] mx-1 bg-stone-300 rounded-full" />
 
-        <div className={cn("flex items-center")}>
+        <div
+          className={cn(
+            expanded ? "flex-row" : "flex-col-reverse",
+            "flex justify-between items-center"
+          )}
+        >
           <Toggle
             onClick={onClickShowSidebarToggle}
             isToggled={expanded}
